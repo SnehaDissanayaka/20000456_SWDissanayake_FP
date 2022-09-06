882bd9634a8b31044eb4a21bbf6b2d6c384c09d7
SW Dissanayake <snehadissanayaka56@gmail.com>
2022-09-06 05:33:36 +0530

    Tutorial 6 answers

--- tutorial_6.scala
+++ tutorial_6.scala
@@ -1,19 +1,22 @@
 object tutorial_6 extends App{
 
-    var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     val Encrypt=(c:Char,key:Int,a:String)=>
-    a((a.indexOf(c.toUpper)+key)%a.size)
+    a((a.indexOf(c.toUpper)+key)%a.size);
 
     val Decrypt=(c:Char,key:Int,a:String)=>
-    a((a.indexOf(c.toUpper)-key)%a.size)
+    a((a.indexOf(c.toUpper)-key)%a.size);
 
     val cipher=(algo:(Char,Int,String)=>
     Char,s:String,key:Int,a:String)=>
-    s.map(algo(_,key,a))
+    s.map(algo(_,key,a));
 
-    val ct=cipher(E,s,5,alphabet)
+    val ct=cipher(Encrypt,text,5,alphabet);
 
-    val pt=cipher(D,ct,5,alphabet)
+    val pt=cipher(Decrypt,ct,5,alphabet);
 
+    val text= "thff";
+
+    println(ct.map(_.toLower));
 }
