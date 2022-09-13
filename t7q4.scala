ff92f76a17c6889d3da89dce3d533a2a2d344366
Sneha Dissanayake <79758855+SnehaDissanayaka@users.noreply.github.com>
2022-09-13 10:26:02 +0530

    Tutorial 7 answers

--- t7q4.scala
+++ t7q4.scala
@@ -5,37 +5,34 @@ object t7q4 extends App {
     def accCreate(nic:String, accId: Int):Unit = {
         val acc = new Account(nic, accId)
         accountList = accountList ::: acc :: Nil
-        println(accountList)
+        printf("Account List : %s\n",accountList)
     }
 
     val find = (a:Int, b:List[Account]) => b.filter(account => account.accId.equals(a))
     val overdraft = (b:List[Account]) => b.filter(account => account.balance < 0.0)
     val totalBalance = (b:List[Account]) => b.foldLeft(0.0)((x, y) => x + y.balance)
-    val interest = (b:List[Account]) => b.map(account => if(account.balance > 0) account.balance*0.05 else account.balance*0.1)
-
-
-    /*              Driver Code                */
+    val interest = (b:List[Account]) => b.map(account => if(account.balance > 0) account.balance+account.balance*0.05 else account.balance-account.balance*0.1)
     
     //create accounts
-    accCreate("1",1)
-    accCreate("2",2)
+    accCreate("197733541287",1)
+    accCreate("199816902376",2)
 
     //deposit money
     find(1, accountList)(0).deposit(1000)
-    println(find(1, accountList)(0))
+    printf("Deposited to: %s\n",find(1, accountList)(0))
 
     //transfer money
     find(1, accountList)(0).transfer(2, 100.0)
-    println(find(2, accountList)(0))
+    printf("Transfered to: %s\n",find(2, accountList)(0))
 
     //list of negative balances
-    println(overdraft(accountList))
+    printf("Overdraft accounts: %s\n",overdraft(accountList))
 
     //sum of all account balances
-    println(totalBalance(accountList))
+    printf("Total balance of all accounts: %f\n",totalBalance(accountList))
 
     //final balances of all accounts after apply the interest
-    println(interest(accountList))
+    printf("List of balances with interests of accounts: %s\n",interest(accountList))
 }
 
 class Account(nic:String, val accId: Int, var balance: Double = 0.0){
