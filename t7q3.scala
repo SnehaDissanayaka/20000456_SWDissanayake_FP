3081426d89c9267a0956435adda7f790417323c2
Sneha Dissanayake <79758855+SnehaDissanayaka@users.noreply.github.com>
2022-09-13 10:25:16 +0530

    Tutorial 7 answers

--- t7q3.scala
+++ t7q3.scala
@@ -4,23 +4,21 @@ object t7q3 extends App {
     def accCreate(nic:String, accId: Int):Unit = {
         val acc = new Account(nic, accId)
         accountList = accountList ::: acc :: Nil
-        println(accountList)
+        printf("Account List : %s\n",accountList)
     }
 
     val find = (a:Int, b:List[Account]) => b.filter(account => account.accId.equals(a))
-
-    /*              Driver Code                */
-    
-    accCreate("1",1)
-    accCreate("2",2)
+  
+    accCreate("200077893421",1)
+    accCreate("200134562309",2)
 
     //deposit money
     find(1, accountList)(0).deposit(1000)
-    println(find(1, accountList)(0))
+    printf("Deposited to: %s\n",find(1, accountList)(0))
 
     //transfer money
     find(1, accountList)(0).transfer(2, 100.0)
-    println(find(2, accountList)(0))
+    printf("Transfered to: %s\n",find(2, accountList)(0))
 }
 
 class Account(nic:String, val accId: Int, var balance: Double = 0.0){
